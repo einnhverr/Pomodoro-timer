d97e10922df0183992d06829f879600950c9efbc
Erhard Muresan <erhard@muresan.ch>
2022-01-25 19:34:29 +0200

    Add LEDs strip.

--- Pomodoro.ino
+++ Pomodoro.ino
@@ -2,11 +2,12 @@
   #error Runs only on ESP32 platform!
 #endif
 
-#include <Wire.h>
-#include <Adafruit_SSD1306.h>
 #include <Adafruit_GFX.h>
-#include <Fsm.h>
+#include <Adafruit_SSD1306.h>
+#include <Adafruit_NeoPixel.h>
 #include <ESPDateTime.h>
+#include <Fsm.h>
+#include <Wire.h>
 
 #define _TIMERINTERRUPT_LOGLEVEL_ 0
 #include <ESP32_New_TimerInterrupt.h>
@@ -21,6 +22,10 @@
 #define OLED_RESET -1
 Adafruit_SSD1306 display(OLED_WIDTH, OLED_HEIGHT, &Wire, OLED_RESET);
 
+#define RING_LED GPIO_NUM_4
+#define RING_LED_SIZE 12
+#define RING_LED_BRIGHTNESS 50
+Adafruit_NeoPixel ring_led(RING_LED_SIZE, RING_LED, NEO_GRB + NEO_KHZ800);
 
 // ESP32 Timer
 ESP32Timer ITimer(1);
@@ -33,14 +38,15 @@ volatile unsigned long interrupt_time;
 #define BUTTON_RIGHT GPIO_NUM_19
 
 // app variables
-int cfg_focus_time = 25 * 60;
-int cfg_break_short_time = 5 * 60;
-int cfg_break_long_time = 15 * 60;
+int cfg_focus_time = 60;//25 * 60;
+int cfg_break_short_time = 12;//5 * 60;
+int cfg_break_long_time = 36;//15 * 60;
 int cfg_break_counter = 4;
 enum Progress {CLEAR, FOCUS_STDBY, FOCUS, FOCUS_PAUSE, BREAK_STDBY, BREAK};
 Progress progress;
 
 // time with 1s resolution
+time_t max_time_counter = cfg_focus_time;
 volatile time_t time_counter = cfg_focus_time;
 volatile int focus_counter = 0;
 
@@ -111,7 +117,7 @@ static const uint8_t pomodoro_bmp[] = {
   0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00
 };
 
-// Finite state machine
+// finite state machine
 // event ids
 #define BUTTON_LEFT_EVENT 1
 #define BUTTON_MIDDLE_EVENT 2
@@ -125,7 +131,22 @@ State state_focus(focus_on_enter, &focus_on_state, &focus_on_exit);
 State state_focus_pause(focus_pause_on_enter, &focus_pause_on_state, &focus_pause_on_exit);
 State state_break_stdby(break_stdby_on_enter, &break_stdby_on_state, &break_stdby_on_exit);
 State state_break(break_on_enter, &break_on_state, &break_on_exit);
-Fsm fsm(&state_clear);
+Fsm fsm_timer(&state_clear);
+
+#define RINGLED_OFF_EVENT 6
+#define RINGLED_ON_EVENT 7
+#define RINGLED_SWITCHCOLOR_EVENT 8
+#define RINGLED_PROGRESSBAR_EVENT 9
+State state_ringled_off(ringled_off_on_enter, &ringled_off_on_state, &ringled_off_on_exit);
+State state_ringled_on(ringled_on_on_enter, &ringled_on_on_state, &ringled_on_on_exit);
+State state_ringled_baseColor(ringled_baseColor_on_enter, &ringled_baseColor_on_state,
+				  &ringled_baseColor_on_exit);
+State state_ringled_fillColor(ringled_fillColor_on_enter, &ringled_fillColor_on_state,
+				  &ringled_fillColor_on_exit);
+State state_ringled_progress(ringled_progress_on_enter, &ringled_progress_on_state, &ringled_progress_on_exit);
+Fsm fsm_ringled(&state_ringled_off);
+uint32_t last_progress_time = 0L;
+uint32_t progress_time;
 
 // ISR
 bool IRAM_ATTR countDown(void *timerNo) {
@@ -137,7 +158,7 @@ bool IRAM_ATTR countDown(void *timerNo) {
 void IRAM_ATTR push_left() {
   interrupt_time = millis();
   if (interrupt_time - last_interrupt_time > 250) {
-    fsm.trigger(BUTTON_LEFT_EVENT);
+    fsm_timer.trigger(BUTTON_LEFT_EVENT);
   }
   last_interrupt_time = interrupt_time;
 }
@@ -145,7 +166,7 @@ void IRAM_ATTR push_left() {
 void IRAM_ATTR push_middle() {
   interrupt_time = millis();
   if (interrupt_time - last_interrupt_time > 250) {
-    fsm.trigger(BUTTON_MIDDLE_EVENT);
+    fsm_timer.trigger(BUTTON_MIDDLE_EVENT);
     }
   last_interrupt_time = interrupt_time;
 }
@@ -153,7 +174,7 @@ void IRAM_ATTR push_middle() {
 void IRAM_ATTR push_right() {
   interrupt_time = millis();
   if (interrupt_time - last_interrupt_time > 250) {
-    fsm.trigger(BUTTON_RIGHT_EVENT);
+    fsm_timer.trigger(BUTTON_RIGHT_EVENT);
   }
   last_interrupt_time = interrupt_time;
 }
@@ -163,11 +184,10 @@ void clear_on_enter() {
   progress = CLEAR;
   focus_counter = 0;
   // initial counter value
-  time_counter = cfg_focus_time;
 }
 
 void clear_on_state() {
-  fsm.trigger(STATE_CLEAR_EVENT);
+  fsm_timer.trigger(STATE_CLEAR_EVENT);
 }
 
 void clear_on_exit() {
@@ -176,9 +196,11 @@ void clear_on_exit() {
 void focus_stdby_on_enter() {
   progress = FOCUS_STDBY;
   time_counter = cfg_focus_time;
+  max_time_counter = cfg_focus_time;
 }
 
 void focus_stdby_on_state() {
+  fsm_ringled.trigger(RINGLED_ON_EVENT);
 }
 
 void focus_stdby_on_exit() {
@@ -190,11 +212,15 @@ void focus_on_enter() {
 }
 
 void focus_on_state() {
+  if (time_counter == 1) {
+    fsm_ringled.trigger(RINGLED_SWITCHCOLOR_EVENT);
+  }
   if (time_counter == 0) {
     ITimer.stopTimer();
     focus_counter++;
-    fsm.trigger(STATE_FINISHED_EVENT);
+    fsm_timer.trigger(STATE_FINISHED_EVENT);
   }
+  fsm_ringled.trigger(RINGLED_PROGRESSBAR_EVENT);
 }
 
 void focus_on_exit() {
@@ -215,12 +241,15 @@ void break_stdby_on_enter() {
   progress = BREAK_STDBY;
   if (focus_counter < 4) {
     time_counter = cfg_break_short_time;
+    max_time_counter = cfg_break_short_time;
   } else {
     time_counter = cfg_break_long_time;
+    max_time_counter = cfg_break_long_time;
   }
 }
 
 void break_stdby_on_state() {
+  fsm_ringled.trigger(RINGLED_ON_EVENT);
 }
 
 void break_stdby_on_exit() {
@@ -232,10 +261,14 @@ void break_on_enter() {
 }
 
 void break_on_state() {
+  if (time_counter == 1) {
+    fsm_ringled.trigger(RINGLED_SWITCHCOLOR_EVENT);
+  }
   if (time_counter == 0) {
     ITimer.stopTimer();
-    fsm.trigger(STATE_FINISHED_EVENT);
+    fsm_timer.trigger(STATE_FINISHED_EVENT);
   }
+  fsm_ringled.trigger(RINGLED_PROGRESSBAR_EVENT);
 }
 
 void break_on_exit() {
@@ -243,6 +276,91 @@ void break_on_exit() {
     focus_counter = 0;
   }
   ITimer.stopTimer();
+}
+
+void ringled_off_on_enter() {
+}
+
+void ringled_off_on_state() {
+  fsm_ringled.trigger(RINGLED_ON_EVENT);
+}
+
+void ringled_off_on_exit() {
+}
+
+void ringled_on_on_enter() {
+  uint32_t baseColor = ring_led.Color(0, 0, 255); // Blue
+  setColor(baseColor);
+}
+
+void ringled_on_on_state() {
+}
+
+void ringled_on_on_exit() {
+}
+
+void ringled_baseColor_on_enter() {
+  uint32_t baseColor = ring_led.Color(0, 0, 255); // Blue
+  setColor(baseColor);
+}
+
+void ringled_baseColor_on_state() {
+}
+
+void ringled_baseColor_on_exit() {
+}
+
+void ringled_fillColor_on_enter() {
+  uint32_t fillColor;
+  switch (progress) {
+  case FOCUS:
+    fillColor = ring_led.Color(255, 0, 0); // Red
+    break;
+  case BREAK:
+    fillColor = ring_led.Color(255, 255, 255, 255); // True white
+    break;
+  default:
+    fillColor = ring_led.Color(0, 0, 0, 255); // True white
+    break;
+  }
+  setColor(fillColor);
+}
+
+void ringled_fillColor_on_state() {
+}
+
+void ringled_fillColor_on_exit() {
+}
+
+void ringled_progress_on_enter() {
+  progress_time = millis();
+  if (progress_time - last_progress_time > 50) {
+    uint32_t baseColor = ring_led.Color(0, 0, 255); // Blue
+    uint32_t fillColor;
+
+    switch (progress) {
+    case FOCUS:
+      fillColor = ring_led.Color(255, 0, 0); // Red
+      break;
+    case FOCUS_PAUSE:
+      fillColor = ring_led.Color(255, 255, 255, 255); // RGB white
+      break;
+    case BREAK:
+      fillColor = ring_led.Color(255, 255, 255, 255); // RGB white
+      break;
+    default:
+      fillColor = ring_led.Color(0, 0, 0, 255); // True white
+      break;
+    }
+    fillProgressBar(baseColor, fillColor, time_counter);
+  }
+  last_progress_time = progress_time;
+}
+
+void ringled_progress_on_state() {
+}
+
+void ringled_progress_on_exit() {
 }
 
 // initial hw setup
@@ -280,27 +398,49 @@ void setup() {
   ITimer.attachInterruptInterval(1000000, countDown);
   ITimer.stopTimer();
 
+  // setup led ring
+  ring_led.begin();
+  ring_led.setBrightness(RING_LED_BRIGHTNESS);
+  ring_led.show();
+
   // FSM transitions
-  fsm.add_transition(&state_clear, &state_focus_stdby, STATE_CLEAR_EVENT, NULL);
+  // timer transitions
+  fsm_timer.add_transition(&state_clear, &state_focus_stdby, STATE_CLEAR_EVENT, NULL);
 
-  fsm.add_transition(&state_focus_stdby, &state_focus, BUTTON_LEFT_EVENT, NULL);
-  fsm.add_transition(&state_focus_stdby, &state_clear, BUTTON_RIGHT_EVENT, NULL);
+  fsm_timer.add_transition(&state_focus_stdby, &state_focus, BUTTON_LEFT_EVENT, NULL);
+  fsm_timer.add_transition(&state_focus_stdby, &state_clear, BUTTON_RIGHT_EVENT, NULL);
 
-  fsm.add_transition(&state_focus, &state_focus_pause, BUTTON_MIDDLE_EVENT, NULL);
-  fsm.add_transition(&state_focus, &state_break_stdby, BUTTON_LEFT_EVENT, NULL);
-  fsm.add_transition(&state_focus, &state_clear, BUTTON_RIGHT_EVENT, NULL);
-  fsm.add_transition(&state_focus, &state_break_stdby, STATE_FINISHED_EVENT, NULL);
+  fsm_timer.add_transition(&state_focus, &state_focus_pause, BUTTON_MIDDLE_EVENT, NULL);
+  fsm_timer.add_transition(&state_focus, &state_break_stdby, BUTTON_LEFT_EVENT, NULL);
+  fsm_timer.add_transition(&state_focus, &state_clear, BUTTON_RIGHT_EVENT, NULL);
+  fsm_timer.add_transition(&state_focus, &state_break_stdby, STATE_FINISHED_EVENT, NULL);
 
-  fsm.add_transition(&state_focus_pause, &state_clear, BUTTON_RIGHT_EVENT, NULL);
-  fsm.add_transition(&state_focus_pause, &state_focus, BUTTON_MIDDLE_EVENT, NULL);
-  fsm.add_transition(&state_focus_pause, &state_break_stdby, BUTTON_LEFT_EVENT, NULL);
+  fsm_timer.add_transition(&state_focus_pause, &state_clear, BUTTON_RIGHT_EVENT, NULL);
+  fsm_timer.add_transition(&state_focus_pause, &state_focus, BUTTON_MIDDLE_EVENT, NULL);
+  fsm_timer.add_transition(&state_focus_pause, &state_break_stdby, BUTTON_LEFT_EVENT, NULL);
   
-  fsm.add_transition(&state_break_stdby, &state_break, BUTTON_LEFT_EVENT, NULL);
-  fsm.add_transition(&state_break_stdby, &state_clear, BUTTON_RIGHT_EVENT, NULL);
+  fsm_timer.add_transition(&state_break_stdby, &state_break, BUTTON_LEFT_EVENT, NULL);
+  fsm_timer.add_transition(&state_break_stdby, &state_clear, BUTTON_RIGHT_EVENT, NULL);
 
-  fsm.add_transition(&state_break, &state_focus_stdby, BUTTON_LEFT_EVENT, NULL);
-  fsm.add_transition(&state_break, &state_clear, BUTTON_RIGHT_EVENT, NULL);
-  fsm.add_transition(&state_break, &state_focus_stdby, STATE_FINISHED_EVENT, NULL);
+  fsm_timer.add_transition(&state_break, &state_focus_stdby, BUTTON_LEFT_EVENT, NULL);
+  fsm_timer.add_transition(&state_break, &state_clear, BUTTON_RIGHT_EVENT, NULL);
+  fsm_timer.add_transition(&state_break, &state_focus_stdby, STATE_FINISHED_EVENT, NULL);
+
+  // ringled transitions
+  fsm_ringled.add_transition(&state_ringled_off, &state_ringled_on, RINGLED_ON_EVENT, NULL);
+
+  fsm_ringled.add_transition(&state_ringled_on, &state_ringled_off, RINGLED_OFF_EVENT, NULL);
+  fsm_ringled.add_transition(&state_ringled_on, &state_ringled_progress, RINGLED_PROGRESSBAR_EVENT, NULL);
+
+  fsm_ringled.add_timed_transition(&state_ringled_progress, &state_ringled_progress, 200, NULL);
+  fsm_ringled.add_transition(&state_ringled_progress, &state_ringled_baseColor, RINGLED_SWITCHCOLOR_EVENT, NULL);
+  fsm_ringled.add_transition(&state_ringled_progress, &state_ringled_on, RINGLED_ON_EVENT, NULL);
+
+  fsm_ringled.add_timed_transition(&state_ringled_baseColor, &state_ringled_fillColor, 200, NULL);
+  fsm_ringled.add_transition(&state_ringled_baseColor, &state_ringled_on, RINGLED_ON_EVENT, NULL);
+
+  fsm_ringled.add_timed_transition(&state_ringled_fillColor, &state_ringled_baseColor, 200, NULL);
+  fsm_ringled.add_transition(&state_ringled_fillColor, &state_ringled_on, RINGLED_ON_EVENT, NULL);
 }
 
 void drawDisplay() {
@@ -413,7 +553,35 @@ void drawSessionCounter(int x, int y, int sessionNo) {
   }
 }
 
+void fillProgressBar(uint32_t baseColor, uint32_t fillColor, uint32_t value) {
+  int i, j;
+  uint32_t a, b, min, max, normalized;
+
+  a = 0;
+  b = ring_led.numPixels();
+  min = 0;
+  max = max_time_counter;
+  normalized = a + ((value - min) * (b - a)) / (max - min);
+
+  Serial.println(b);
+  Serial.println(b - normalized);
+
+  for (i = 0; i < b - normalized + 1; ++i) {
+    ring_led.setPixelColor(i, fillColor);
+  }
+  ring_led.show();
+}
+
+void setColor(uint32_t color) {
+  int size = ring_led.numPixels();
+  for (int i = 0; i < size; ++i) {
+    ring_led.setPixelColor(i, color);
+  }
+  ring_led.show();
+}
+
 void loop() {
-  fsm.run_machine();
+  fsm_timer.run_machine();
+  fsm_ringled.run_machine();
   drawDisplay();
 }
